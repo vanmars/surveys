31176fff5599bd5e32ffa6f292a07d468f0c823c
Vanessa Stewart <vstewart1213@gmail.com>
2020-10-13 22:21:34 -0700

    Add callback to titleize survey title

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -2,4 +2,10 @@ class Survey < ApplicationRecord
   has_many :questions, dependent: :destroy
 
   validates :title, presence: true
+
+  before_save(:titleize_survey)
+    private
+      def titleize_survey
+        self.title = self.title.titlecase
+      end
 end
--- spec/models/survey_spec.rb
+++ spec/models/survey_spec.rb
@@ -3,4 +3,8 @@ require 'rails_helper'
 describe Survey do
   it { should have_many(:questions)}
   it { should validate_presence_of :title}
+  it('titleizes the tle of the survey') do
+    survey = Survey.create({title: 'favorite books'})
+    expect(survey.title).to(eq("Favorite Books"))
+  end
 end
